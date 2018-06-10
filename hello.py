1c5fff6eb24e58b6f55fe835fea815699b1b5518
ArthurMinSein <arthurmin93@gmail.com>
2018-06-10 16:00:39 +0630

    add a new route

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def arthur():
-    return "Hello, Arthur\nGo to <a href"https://tiide3.herokuapp.com/tiide">TIIDE</a>"
+    return 'Hello, Arthur\nGo to <a href"https://tiide3.herokuapp.com/tiide">TIIDE</a>'
 
 @app.route("/tiide")
 def tiide():
